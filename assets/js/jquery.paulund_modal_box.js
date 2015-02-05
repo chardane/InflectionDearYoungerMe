ac21328edcdf588bc35fdaa531378739b5c59dcf
BigSuave <daviddcchatman@gmail.com>
2015-02-05 14:53:38 -0800

    Added a modal window for when you click on the video, Added the play button over video img

---
+++ assets/js/jquery.paulund_modal_box.js
@@ -0,0 +1,94 @@
+(function($){
+
+	// Defining our jQuery plugin
+
+	$.fn.paulund_modal_box = function(prop){
+
+		// Default parameters
+
+		var options = $.extend({
+			height : "380px",
+			width : "500px",
+			top: "40%",
+			left: "29%",
+		},prop);
+				
+		return this.click(function(e){
+			add_block_page();
+			add_popup_box();
+			add_styles();
+			
+			$('.paulund_modal_box').fadeIn();
+		});
+		
+		 function add_styles(){			
+			$('.paulund_modal_box').css({ 
+				'position':'fixed', 
+				'left':options.left,
+				'top':options.top,
+				'display':'none',
+				'border':'1px solid #fff',
+				'box-shadow': '0px 2px 7px #292929',
+				'-moz-box-shadow': '0px 2px 7px #292929',
+				'-webkit-box-shadow': '0px 2px 7px #292929',
+				'border-radius':'10px',
+				'-moz-border-radius':'10px',
+				'-webkit-border-radius':'10px',
+				'background': '#3c3d4d', 
+				'z-index':'50',
+			});
+			$('.paulund_modal_close').css({
+				'position':'relative',
+				'top':'-25px',
+				'left':'20px',
+				'float':'right',
+				'display':'block',
+				'height':'50px',
+				'width':'50px',
+				'background': 'url(../assets/imgs/landing/close.png) no-repeat',
+			});
+                        /*Block page overlay*/
+			var pageHeight = $(document).height();
+			var pageWidth = $(window).width();
+
+			$('.paulund_block_page').css({
+				'position':'absolute',
+				'top':'0',
+				'left':'0',
+				'background-color':'rgba(255,255,255,0.63)',
+				'height':pageHeight,
+				'width':pageWidth,
+				'z-index':'10'
+			});
+			$('.paulund_inner_modal_box').css({
+				'background-color':'#fff',
+				'height':(options.height - 50) + 'px',
+				'width':(options.width - 50) + 'px',
+				'padding':'10px',
+				'margin':'15px',
+				'border-radius':'10px',
+				'-moz-border-radius':'10px',
+				'-webkit-border-radius':'10px'
+			});
+		}
+		
+		 function add_block_page(){
+			var block_page = $('<div class="paulund_block_page"></div>');
+						
+			$(block_page).appendTo('body');
+		}
+		 		
+		 function add_popup_box(){
+			 var pop_up = $('<video class="paulund_modal_box" src="../assets/vids/DearYoungerMe.m4v" controls height="480px" width="800px"><a href="#" class="paulund_modal_close"></a><h2>' + '</h2><p>' + options.description + '</p></video>');
+			 $(pop_up).appendTo('.paulund_block_page');
+			 			 
+			 $('.paulund_modal_close').click(function(){
+				$(this).parent().fadeOut().remove();
+				$('.paulund_block_page').fadeOut().remove();				 
+			 });
+		}
+
+		return this;
+	};
+	
+})(jQuery);
